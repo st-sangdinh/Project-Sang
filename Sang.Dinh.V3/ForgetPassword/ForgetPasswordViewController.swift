cf3ae1e5133692d91df41925defe1dd158f4efda
rinsang2000 <rinsang2000@gmai.com>
2022-07-06 11:39:13 +0700

    Create UI ForgetPassword

---
+++ Sang.Dinh.V3/ForgetPassword/ForgetPasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForgetPasswordViewController.swift
+//  Sang.Dinh.V3
+//
+//  Created by Rin Sang on 06/07/2022.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sang.Dinh.V3/ForgetPassword/ForgetPasswordViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ForgetPasswordViewModel.swift
+//  Sang.Dinh.V3
+//
+//  Created by Rin Sang on 06/07/2022.
+//
+
+import Foundation
